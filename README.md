e617bb44c4eccdeddd9b6fb38cbeeef24369de61
Ahmad Baseer <ahmedbaseer871@gmail.com>
2024-09-04 17:56:41 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+---
+title: Europe Based Hotel Recommendation System
+emoji: 😻
+colorFrom: blue
+colorTo: gray
+sdk: streamlit
+sdk_version: 1.38.0
+app_file: Web_App.py
+pinned: false
+---
+
+Status Badge: [![Sync to Hugging Face hub](https://github.com/Ahmad-Baseer/Europe-Based-Hotel-Recommendation-System/actions/workflows/CD_to_HuggingFace.yml/badge.svg)](https://github.com/Ahmad-Baseer/Europe-Based-Hotel-Recommendation-System/actions/workflows/CD_to_HuggingFace.yml)
+
+# How to run the application
+
+
+### Follow the steps to configure this application:
+
+1. Download all the files and place them in a specific folder. Open up Jupyter Notebook in your working folder.
+2. Install the libraries first before running notebooks. Remove the ```#``` sign from the lines to allow nltk to download required files & to see if everything works fine or not.
+3. Download the Hotel Reviews dataset from [here](https://www.kaggle.com/datasets/jiashenliu/515k-hotel-reviews-data-in-europe)
+4. Run the **Hotel_Recommendation_System** file first and then **Hotel Recommendation System Web App**, after running the second one you should see this: **Overwriting Web_App.py** in output.
+5. Open up the terminal in the same folder and type this command: ```streamlit run Web_App.py```. It will open your application in your default browser.
+6. Now, you can follow any YouTube video to deploy this application.
+7. You can use my application here: [Europe-based-hotel-recommendation-system](https://huggingface.co/spaces/AhmadHashim/Europe-based-hotel-recommendation-system)
+
+## Thanks for reading
+
---
+++ Web_App.py
@@ -0,0 +1,98 @@
+import streamlit as st
+from streamlit_lottie import st_lottie
+import requests as req
+import import_ipynb
+from Hotel_Recommendation_System import recommender
+
+st.set_page_config(page_title="Hotel Recommendation System",page_icon=":hotel:",layout="wide")
+
+with st.container():
+    st.title("Welcome to Hotel Recommendation System :hotel:")
+    st.subheader("Author: Ahmad Baseer")
+    st.write("You can find the code [here](https://github.com/Ahmad-Baseer/AI-Projects)")
+
+def ret_country(country):
+    if(country=="Netherlands"):
+        return "NL"
+    if(country=="United Kingdom"):
+        return "UK"
+    if(country=="France"):
+        return "FR"
+    if(country=="Spain"):
+        return "ES"
+    if(country=="Italy"):
+        return "IT"
+    if(country=="Austria"):
+        return "AT"
+
+def load_lottieurl(url):
+    r=req.get(url)
+    if r.status_code !=200:
+        None
+    return r.json()
+
+lottie_hotel=load_lottieurl("https://assets3.lottiefiles.com/packages/lf20_xrptzsed.json")
+    
+with st.container():
+    st.write("---")
+    left_col,midl_col,midr_col,right_col=st.columns(4)
+    
+    with left_col:
+        st.write("##")
+        st.write("##")
+        country=ret_country(st.selectbox("Country",('Netherlands','United Kingdom','France','Spain','Italy','Austria')))
+        st.write(country)
+        
+    with midl_col:
+        st.write("##")
+        st.write("##")
+        tags=st.selectbox("Tags",("Business trip","Leisure trip"))
+        st.write(tags)
+        
+    with midr_col:
+        st.write("##")
+        st.write("##")
+        category=st.selectbox("Category",("Couple","Solo Traveller","With a pet","Group","Family with young children",
+                                         "Family with older children","Travelers with friends"))
+        st.write(category)
+        
+    with right_col:
+        st_lottie(lottie_hotel,height=250,key="hotel")
+        
+    with midl_col:
+        st.write("##")
+        button=st.button("Recommend",help="Will Recommend hotels based on their ratings!")
+        
+    with st.container():
+        if(button):
+            st.dataframe(recommender(country,tags+'  '+category))
+            st.balloons()
+            st.snow()
+        
+    with st.container():
+        st.write("##")
+        st.header("Get in Touch with me!")
+        st.write("##")
+        contact_form="""
+        <form action="https://formsubmit.co/asfars305@gmail.com" method="POST">
+     <input type="hidden" name="_captcha" value="false" required>
+     <input type="text" name="name" placeholder="Your Name" required>
+     <input type="email" name="email" placeholder="Your Email" required>
+     <textarea name="message" placeholder="Your Message Here" required></textarea>
+     <button type="submit">Send</button>
+     </form>
+        """
+        ul_col,ur_col=st.columns(2)
+        
+        with ul_col:
+            st.markdown(contact_form, unsafe_allow_html=True)
+            
+        with ur_col:
+            st.empty()
+            
+#using local css to design contact form
+def local_css_for_contact_form(file_name):
+    with open(file_name) as f:
+        st.markdown(f"<style>{f.read()}</style>",unsafe_allow_html=True)
+        
+local_css_for_contact_form("style.css")
